dfea08326cdafd2d615fcff9e513c0a9d87bc293
Prateek1013 <prateek9771196112@gmail.com>
2023-12-27 02:36:11 +0530

    Tab title changed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -49,17 +49,12 @@ const About = () => {
       >
         I'm a skilled software developer with experience in TypeScript and
         JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
+        Next.js. I'm a quick learner and collaborate closely with clients to
         create efficient, scalable, and user-friendly solutions that solve
         real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
       
       <Tech/>
-      {/* <div className='mt-20 flex flex-wrap gap-10'>
-        {services.map((service, index) => (
-          <ServiceCard key={service.title} index={index} {...service} />
-        ))}
-      </div> */}
     </>
   );
 };
